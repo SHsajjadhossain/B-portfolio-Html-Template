17ba5ff61f7ceaf67d6dd252fb60995ee3f6b1c4
Sajjad Hossain <sajjadhossain.hs@gmail.com>
2024-01-18 21:35:15 +0600

    commit

--- js/script.js
+++ js/script.js
@@ -42,11 +42,14 @@ $('.port-slider').slick({
 // ==============================================
 
 $('.service-slider').slick({
-  dots: true,
+  dots: false,
   infinite: true,
+  autoplay: true,
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 1,
+  prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
+  nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
 });
 
 // ==============================================
